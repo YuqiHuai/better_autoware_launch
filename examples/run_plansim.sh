afb6784bbdd589fbd3584b863c45ec8afc6f9ef4
Yuqi Huai <yhuai@uci.edu>
2025-02-14 14:59:43 -0800

    feat: add sample scripts

---
+++ examples/run_plansim.sh
@@ -0,0 +1,3 @@
+ros2 launch autoware_launch planning_simulator.launch.xml \
+    map_path:=$HOME/autoware_map/sample-map-planning \
+    vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
